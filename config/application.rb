68a434201d9a1a2e9d8b3e4ccd254e0acd053cdd
Bhaarat Sharma <bhaarat.s@gmail.com>
2011-05-07 14:03:03 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,6 @@ module Dentalassistant
 	  config.middleware.use  "PDFKit::Middleware", :print_media_type => true
     #test
     if ::Rails.env == 'production'
-      config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64');
     end
   end
 end
