702767563d9cbea98965a77a2df1751f94423a0e
Bhaarat Sharma <bhaarat.s@gmail.com>
2011-05-07 14:19:20 -0400

    pdfkit

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 # config/initializers/pdfkit.rb  
   
 PDFKit.configure do |config|       
-     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if ::Rails.env == 'production'  
+     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf').to_s if ::Rails.env == 'production'  
 end  
