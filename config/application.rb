69b7e46cd034a031c33c8ba83c1b4f0ab82a0741
Bhaarat Sharma <bhaarat.s@gmail.com>
2011-05-07 14:15:07 -0400

    removed duplication

--- config/application.rb
+++ config/application.rb
@@ -40,8 +40,5 @@ module Dentalassistant
     config.filter_parameters += [:password]
 	  config.middleware.use  "PDFKit::Middleware", :print_media_type => true
     #test
-    if ::Rails.env == 'production'
-      config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64');
-    end
   end
 end
--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 # config/initializers/pdfkit.rb  
   
 PDFKit.configure do |config|       
-     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'  
+     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if ::Rails.env == 'production'  
 end  
