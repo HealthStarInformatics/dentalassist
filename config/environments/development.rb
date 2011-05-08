09f1bb4f4abb9981d9a57326da14766d29ef3dc9
Bhaarat Sharma <bhaarat.s@gmail.com>
2011-05-08 09:03:53 -0400

    changed age to readonly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,11 +23,7 @@ Dentalassistant::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_controller.asset_host = Proc.new do |request, source|
-    if request.include? '.pdf'
-     print "file://#{Rails.root.join('public')}" 
-    end
-  end
-
+  config.action_controller.asset_host = Proc.new { |source,request|
+  source.match(/pdf/) ? "file://#{Rails.root.join('public')}" : "#{request.protocol}#{request.host_with_port}"}
 end
 
